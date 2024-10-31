997070969ba267121472c536c3b1774c39ea688f
Victor Chizhov <chizhovvs17@gmail.com>
2024-10-31 19:17:49 +0300

    Add icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,31 +1,147 @@
 import SvgIconAbout from './about.svg?raw'
 import SvgIconArrowLeft from './arrow-left.svg?raw'
+import SvgIconBebo from './bebo.svg?raw'
+import SvgIconBinoculars from './binoculars.svg?raw'
+import SvgIconBookWithBookmark from './book-with-bookmark.svg?raw'
 import SvgIconBook from './book.svg?raw'
+import SvgIconBookmark from './bookmark.svg?raw'
+import SvgIconBox from './box.svg?raw'
+import SvgIconBriefcase from './briefcase.svg?raw'
+import SvgIconCancel from './cancel.svg?raw'
+import SvgIconCheckmarkCircle from './checkmark-circle.svg?raw'
+import SvgIconCheckmark from './checkmark.svg?raw'
+import SvgIconClock from './clock.svg?raw'
+import SvgIconClose from './close.svg?raw'
+import SvgIconConnect from './connect.svg?raw'
+import SvgIconCursor from './cursor.svg?raw'
+import SvgIconDelete from './delete.svg?raw'
 import SvgIconDocument from './document.svg?raw'
+import SvgIconDone from './done.svg?raw'
+import SvgIconDribble from './dribble.svg?raw'
+import SvgIconEdit from './edit.svg?raw'
+import SvgIconExternalLink from './external-link.svg?raw'
+import SvgIconFacebookCircle from './facebook-circle.svg?raw'
+import SvgIconFacebook from './facebook.svg?raw'
+import SvgIconFile from './file.svg?raw'
+import SvgIconFolder from './folder.svg?raw'
+import SvgIconGooglePlus from './google-plus.svg?raw'
+import SvgIconHandCursor from './hand-cursor.svg?raw'
 import SvgIconHome from './home.svg?raw'
+import SvgIconIdea from './idea.svg?raw'
+import SvgIconImageFile from './image-file.svg?raw'
+import SvgIconInfo from './info.svg?raw'
+import SvgIconInstagramOld from './instagram-old.svg?raw'
+import SvgIconInstagram from './instagram.svg?raw'
+import SvgIconKey from './key.svg?raw'
+import SvgIconLinkedin from './linkedin.svg?raw'
 import SvgIconLock from './lock.svg?raw'
 import SvgIconLogout from './logout.svg?raw'
+import SvgIconLunacy from './lunacy.svg?raw'
 import SvgIconMailbox from './mailbox.svg?raw'
 import SvgIconMenu from './menu.svg?raw'
+import SvgIconMusic from './music.svg?raw'
+import SvgIconMyspaceApp from './myspace-app.svg?raw'
+import SvgIconMyspace from './myspace.svg?raw'
 import SvgIconNews from './news.svg?raw'
+import SvgIconNoSynchronize from './no-synchronize.svg?raw'
+import SvgIconPadlock from './padlock.svg?raw'
+import SvgIconPinterest from './pinterest.svg?raw'
+import SvgIconPlus from './plus.svg?raw'
+import SvgIconPuzzle from './puzzle.svg?raw'
+import SvgIconReddit from './reddit.svg?raw'
+import SvgIconRestart from './restart.svg?raw'
+import SvgIconRound from './round.svg?raw'
+import SvgIconSearch from './search.svg?raw'
+import SvgIconServices from './services.svg?raw'
 import SvgIconSettings from './settings.svg?raw'
 import SvgIconShare from './share.svg?raw'
+import SvgIconSpeechBubble from './speech-bubble.svg?raw'
+import SvgIconStumbleupon from './stumbleupon.svg?raw'
+import SvgIconSun from './sun.svg?raw'
+import SvgIconSupport from './support.svg?raw'
 import SvgIconSynchronize from './synchronize.svg?raw'
+import SvgIconTickBox from './tick-box.svg?raw'
+import SvgIconTinder from './tinder.svg?raw'
+import SvgIconToolbox from './toolbox.svg?raw'
+import SvgIconTrashCan from './trash-can.svg?raw'
+import SvgIconTrash from './trash.svg?raw'
+import SvgIconTumblr from './tumblr.svg?raw'
+import SvgIconTwitter from './twitter.svg?raw'
+import SvgIconUncheckAll from './uncheck-all.svg?raw'
+import SvgIconUserFemale from './user-female.svg?raw'
 import SvgIconUserMale from './user-male.svg?raw'
 
 export const iconsPack = {
   about: SvgIconAbout,
   arrow_left: SvgIconArrowLeft,
+  bebo: SvgIconBebo,
+  binoculars: SvgIconBinoculars,
+  book_with_bookmark: SvgIconBookWithBookmark,
   book: SvgIconBook,
+  bookmark: SvgIconBookmark,
+  box: SvgIconBox,
+  briefcase: SvgIconBriefcase,
+  cancel: SvgIconCancel,
+  checkmark_circle: SvgIconCheckmarkCircle,
+  checkmark: SvgIconCheckmark,
+  clock: SvgIconClock,
+  close: SvgIconClose,
+  connect: SvgIconConnect,
+  cursor: SvgIconCursor,
+  delete: SvgIconDelete,
   document: SvgIconDocument,
+  done: SvgIconDone,
+  dribble: SvgIconDribble,
+  edit: SvgIconEdit,
+  external_link: SvgIconExternalLink,
+  facebook_circle: SvgIconFacebookCircle,
+  facebook: SvgIconFacebook,
+  file: SvgIconFile,
+  folder: SvgIconFolder,
+  google_plus: SvgIconGooglePlus,
+  hand_cursor: SvgIconHandCursor,
   home: SvgIconHome,
+  idea: SvgIconIdea,
+  image_file: SvgIconImageFile,
+  info: SvgIconInfo,
+  instagram_old: SvgIconInstagramOld,
+  instagram: SvgIconInstagram,
+  key: SvgIconKey,
+  linkedin: SvgIconLinkedin,
   lock: SvgIconLock,
   logout: SvgIconLogout,
+  lunacy: SvgIconLunacy,
   mailbox: SvgIconMailbox,
   menu: SvgIconMenu,
+  music: SvgIconMusic,
+  myspace_app: SvgIconMyspaceApp,
+  myspace: SvgIconMyspace,
   news: SvgIconNews,
+  no_synchronize: SvgIconNoSynchronize,
+  padlock: SvgIconPadlock,
+  pinterest: SvgIconPinterest,
+  plus: SvgIconPlus,
+  puzzle: SvgIconPuzzle,
+  reddit: SvgIconReddit,
+  restart: SvgIconRestart,
+  round: SvgIconRound,
+  search: SvgIconSearch,
+  services: SvgIconServices,
   settings: SvgIconSettings,
   share: SvgIconShare,
+  speech_bubble: SvgIconSpeechBubble,
+  stumbleupon: SvgIconStumbleupon,
+  sun: SvgIconSun,
+  support: SvgIconSupport,
   synchronize: SvgIconSynchronize,
+  tick_box: SvgIconTickBox,
+  tinder: SvgIconTinder,
+  toolbox: SvgIconToolbox,
+  trash_can: SvgIconTrashCan,
+  trash: SvgIconTrash,
+  tumblr: SvgIconTumblr,
+  twitter: SvgIconTwitter,
+  uncheck_all: SvgIconUncheckAll,
+  user_female: SvgIconUserFemale,
   user_male: SvgIconUserMale,
 }
